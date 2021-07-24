6b3ca1d57ffc190c402eadeb57a7c65c8e3e4571
Shourya <shouryasinha001@gmail.com>
2021-07-24 17:45:14 +0530

    basic shop ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react"
 import './App.css';
-import { Furniture } from "./components/furniture"
+import { Shop } from "./components/shop"
 
 function App() {
   return (
 
     <div className="App">
-
-      <Furniture img="https://images-na.ssl-images-amazon.com/images/I/81K8cuT1QDL._AC_SL1500_.jpg"
-        name="Item"
-        price="$79.99"
-        detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
-         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
-         doloremque odio saepe at libero mollitia? Cupiditate?" />
-
+      <Shop />
     </div>
 
 
--- src/components/shop.js
+++ src/components/shop.js
@@ -1,36 +1,67 @@
 import React from 'react'
 import { Furniture } from "./furniture"
+
 export const Shop = () => {
     return (
-        <>
-            <div className="row">
-                <Furniture
-                    img="https://th.bing.com/th/id/R.448ebec98748ec32c07a6de7dbcd2eb9?rik=d2YQCR%2bUq6ufHg&riu=http%3a%2f%2farchitecturesideas.com%2fwp-content%2fuploads%2f2017%2f05%2f05-28.jpg&ehk=vREnj5jEQV%2bq7On4lkA8WtpTm4btiHMpcKIoleZTn%2b8%3d&risl=&pid=ImgRaw"
-                    name="Wooden Chair"
-                    price="$59.99" />
-                <Furniture
-                    img="https://images-na.ssl-images-amazon.com/images/I/81K8cuT1QDL._AC_SL1500_.jpg"
-                    name="Book shelf"
-                    price="$79.99"  />
-                <Furniture
-                    img="https://th.bing.com/th/id/R.448ebec98748ec32c07a6de7dbcd2eb9?rik=d2YQCR%2bUq6ufHg&riu=http%3a%2f%2farchitecturesideas.com%2fwp-content%2fuploads%2f2017%2f05%2f05-28.jpg&ehk=vREnj5jEQV%2bq7On4lkA8WtpTm4btiHMpcKIoleZTn%2b8%3d&risl=&pid=ImgRaw"
-                    name="Wooden Chair"
-                    price="$79.99" />
-            </div>
-            <div className="row">
-                <Furniture
-                    img="https://th.bing.com/th/id/R.448ebec98748ec32c07a6de7dbcd2eb9?rik=d2YQCR%2bUq6ufHg&riu=http%3a%2f%2farchitecturesideas.com%2fwp-content%2fuploads%2f2017%2f05%2f05-28.jpg&ehk=vREnj5jEQV%2bq7On4lkA8WtpTm4btiHMpcKIoleZTn%2b8%3d&risl=&pid=ImgRaw"
-                    name="Wooden Chair"
-                    price="$79.99" />
-                <Furniture
-                    img="https://th.bing.com/th/id/R.448ebec98748ec32c07a6de7dbcd2eb9?rik=d2YQCR%2bUq6ufHg&riu=http%3a%2f%2farchitecturesideas.com%2fwp-content%2fuploads%2f2017%2f05%2f05-28.jpg&ehk=vREnj5jEQV%2bq7On4lkA8WtpTm4btiHMpcKIoleZTn%2b8%3d&risl=&pid=ImgRaw"
-                    name="Wooden Chair"
-                    price="$79.99" />
-                <Furniture
-                    img="https://th.bing.com/th/id/R.448ebec98748ec32c07a6de7dbcd2eb9?rik=d2YQCR%2bUq6ufHg&riu=http%3a%2f%2farchitecturesideas.com%2fwp-content%2fuploads%2f2017%2f05%2f05-28.jpg&ehk=vREnj5jEQV%2bq7On4lkA8WtpTm4btiHMpcKIoleZTn%2b8%3d&risl=&pid=ImgRaw"
-                    name="Wooden Chair"
-                    price="$79.99" />
-            </div>
-        </>
+        <div className="main">
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/81K8cuT1QDL._AC_SL1500_.jpg"
+                name="Bookshelf"
+                price="$79.99"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/61o2dXPWi3L._AC_SL1200_.jpg"
+                name="Office Chair"
+                price="$109.24"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/71wC7OKtS2L._AC_SL1500_.jpg"
+                name="Shoe rack"
+                price="$24.86"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/81lGjABXq%2BS._AC_SL1500_.jpg"
+                name="Dresser"
+                price="$62.87"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/61mmhQcG0sS._AC_SL1500_.jpg"
+                name="Picture Frame"
+                price="$7.27"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://m.media-amazon.com/images/I/91HUri7OB0L._AC_SL1500_.jpg"
+                name="Armchair"
+                price="$102.99"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+            <Furniture img="https://images-na.ssl-images-amazon.com/images/I/61-nYtfzswL._AC_SL1000_.jpg"
+                name="Vanity Table"
+                price="$132.44"
+                detail="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Eveniet atque minima eligendi porro. Sapiente corrupti ratione iure expedita, 
+         inventore ad id est at tenetur asperiores sed. Dolore accusantium, ipsum dolor 
+         doloremque odio saepe at libero mollitia? Cupiditate?" />
+
+        </div>
     )
 }
+
+
