b2ab2d18ba7c9cdb77a830e0b958f593124c5dfe
Shourya <shouryasinha001@gmail.com>
2021-07-26 21:01:58 +0530

    Home page design ready

--- src/components/deal.js
+++ src/components/deal.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./styles/deal.css"
 export const Deal = (props) => {
-    //{ Name , Discount , Price , pic } 
+    //{ Name , Cross , Price , pic } 
     return (
         <div className="deal">
+            <img src="https://th.bing.com/th/id/OIP.q5osVi_MaQ46JjwTkZXkMAHaEo?pid=ImgDet&w=1920&h=1200&rs=1" alt="" />
+            <h6>Item name</h6>
+            <div className="price">
+                <h5 className="strike">$100</h5>
+                <h5>$80</h5>
+            </div>
+
+            <h3> Deal expires soon! </h3>
 
         </div>
     )
