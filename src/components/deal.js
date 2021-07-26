c35a0e457af0529b5e9400c5458078c314ae53dc
Shourya <shouryasinha001@gmail.com>
2021-07-26 20:39:36 +0530

    Home page design ready

---
+++ src/components/deal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles/deal.css"
+export const Deal = (props) => {
+    //{ Name , Discount , Price , pic } 
+    return (
+        <div className="deal">
+
+        </div>
+    )
+}
--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import "./styles/home.css"
+import { Deal } from "./deal"
 export const Home = () => {
     return (
-        <div>
+        <div className="home-div">
+            <p>Today's top deals</p>
+            <div className="row">
+
+                <Deal />
+                <Deal />
+
+            </div>
+
+            <div className="row">
+
+                <Deal />
+                <Deal />
+
+            </div>
+
+            <footer>
+                Copyright © All rights reserved.
+            </footer>
 
         </div>
     )
