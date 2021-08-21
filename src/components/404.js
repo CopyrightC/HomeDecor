6a553f2ffbdd25fb85282acbff314fdcdfc16cd5
Shourya <shouryasinha001@gmail.com>
2021-08-21 17:38:21 +0530

    Added 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -3,7 +3,7 @@ import "./styles/404.css"
 export const PageDoesntExists = () => {
     return (
         <div id="error">
-            404
+            <p className="errormsg"> Go to <a href="/">home</a> page</p>
         </div>
     )
 }
